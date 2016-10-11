bbd37e71ec58cf5ecc594a7aaf933c9e54226d02
Andre Figueiredo <afigueiredo@opsolve.com>
2016-10-11 19:45:11 -0300

    applied theme and routes made more meaningful

--- src/app/app.js
+++ src/app/app.js
@@ -1,28 +1,29 @@
 var module = angular.module('mailbox', ['templates', 'ui.router']);
 
-module.config(['$stateProvider', '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+module.config(function ($stateProvider, $urlRouterProvider) {
   var mailboxState = {
     name: 'mailbox',
-    url: '/',
+    url: '/messages',
     template: '<mailbox></mailbox>'
   };
   var messageState = {
     name: 'message',
-    url: '/:id',
+    url: '/messages/:id',
     template: '<mailbox></mailbox>'
   };
 
   $stateProvider.state(messageState);
   $stateProvider.state(mailboxState);
-  $urlRouterProvider.otherwise('/');
-}]);
+
+  $urlRouterProvider.otherwise('/messages');
+
+  //$locationProvider.html5Mode({ enabled: true, requireBase: false});
+});
 
 module.component('mailbox', {
   templateUrl: "mailbox/mailbox.html",
   controller: function ($http) {
     var vm = this;
-
     $http.get('../data/mailbox.json').then(function (response) {
       vm.messages = response.data.messages;
     });
@@ -43,10 +44,11 @@ module.component('message', {
   templateUrl: "mailbox/message/message.html",
   controller: function ($http, $stateParams) {
     var vm = this;
-    $http.get('../data/mailbox.json').then(function (response) {
+    $http.get('../data/mailbox.json', {cache: true}).then(function (response) {
       vm.message = response.data.messages.find(function (msg) {
         return msg.uid == $stateParams.id;
       });
     });
   }
 });
+
